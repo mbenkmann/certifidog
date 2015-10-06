d71ef439e6be9b6ef83f06aa88cdd82d6e869718
Matthias S. Benkmann <mux2005@gmail.com>
2015-10-06 13:08:23 +0200

    more code in analysekey

--- test/analysekey.go
+++ test/analysekey.go
@@ -5,6 +5,8 @@ import (
          "os"
          "fmt"
          "crypto/x509"
+         "crypto/rsa"
+         "crypto/ecdsa"
          
          "../asn1"
          "../util"
@@ -209,8 +211,31 @@ func main() {
   key2, err2 := x509.ParsePKCS1PrivateKey(data)
   key3, err3 := x509.ParsePKCS8PrivateKey(data)
   
-  if err1 == nil && key1 != nil { fmt.Fprintf(os.Stdout, "ParseECPrivateKey OK\n") }
-  if err2 == nil && key2 != nil { fmt.Fprintf(os.Stdout, "ParsePKCS1PrivateKey OK\n") }
-  if err3 == nil && key3 != nil { fmt.Fprintf(os.Stdout, "ParsePKCS8PrivateKey OK => %T\n", key3) }
+  var pub interface{}
+  if err1 == nil && key1 != nil { 
+    fmt.Fprintf(os.Stdout, "ParseECPrivateKey OK\n") 
+    pub = key1.Public()
+  }
+  if err2 == nil && key2 != nil { 
+    fmt.Fprintf(os.Stdout, "ParsePKCS1PrivateKey OK\n") 
+    pub = key2.Public()
+  }
+  if err3 == nil && key3 != nil {
+    fmt.Fprintf(os.Stdout, "ParsePKCS8PrivateKey OK => %T\n", key3) 
+    switch key := key3.(type){
+      case *rsa.PrivateKey: pub = key.Public()
+      case *ecdsa.PrivateKey: pub = key.Public()
+    }
+  }
+  
+  if pub != nil {
+    pubkeybytes, err := x509.MarshalPKIXPublicKey(pub)
+    if err != nil {
+      fmt.Fprintf(os.Stderr, "%v\n", err)
+      os.Exit(1)
+    }
+    
+    fmt.Fprintf(os.Stdout, "\nCORRESPONDING PUBLIC KEY: %v\n", asn1.AnalyseDER(pubkeybytes))
+  }
 }
 
