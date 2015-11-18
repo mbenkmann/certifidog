a86b59d35706a95c630177e8b5d929064016ba3b
Matthias S. Benkmann <mux2005@gmail.com>
2015-11-18 10:22:04 +0100

    Support string as data argument to write()

--- main/certificate-assembler.go
+++ main/certificate-assembler.go
@@ -158,11 +158,12 @@ func writeimpl(stack_ *[]*asn1.CookStackElement, location string, overwrite bool
   data2, ok2 := stack[len(stack)-2].Value.([]byte)
   file2, ok3 := stack[len(stack)-1].Value.(string)
   file1, ok4 := stack[len(stack)-2].Value.(string)
-  if !((ok1 && ok4) || (ok2 && ok3)) {
-    return fmt.Errorf("%vwrite() called, but top 2 elements of stack are not a byte-array and a file name", location)
+  if !((ok1 && ok4) || (ok2 && ok3) || (ok3 && ok4)) {
+    return fmt.Errorf("%vwrite() called, but top 2 elements of stack are not a byte-array or string and a file name", location)
   }
   
   if ok2 { data1, file1 = data2, file2 }
+  if ok3 && ok4 { data1, file1 = []byte(file1), file2 }
   
   flag := os.O_WRONLY | os.O_CREATE
   if overwrite { flag = flag | os.O_TRUNC } else { flag = flag | os.O_EXCL }
